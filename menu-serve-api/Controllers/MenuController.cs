1af44a2d5af7229f39f83544c01baba027d8f003
Wayne Mercer <wimercer@outlook.com>
2018-05-25 08:33:50 +0100

    added controller end points for menu

---
+++ menu-serve-api/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace menu_serve_api.Controllers
+{
+    // Route prefix for controller
+    //Get: {mydomain}/api/menu/
+    [Route("api/[controller]")]
+    public class MenuController : Controller
+    {
+        //Get: {mydomain}/api/menu/
+        [HttpGet]
+        public JsonResult Index()
+        {
+            return new JsonResult("Stub for menu data");
+        }
+
+        //Get: {mydomain}/api/menu/1
+        [Route("item/{id}")]
+        public JsonResult Item(int id)
+        {
+
+            return new JsonResult("Menu Item " + id.ToString());
+        }
+    }
+}
---
+++ menu-serve-api/DatabaseScripts/build.sql
@@ -0,0 +1,20 @@
+﻿declare @dbname varchar(max)
+set @dbname = 'menu-serve'
+
+use [Master]
+go
+
+if exists
+	(	
+	select 
+		* 
+	from 
+		sys.databases 
+	where
+		name = [@dbname]
+	)
+
+drop database [@dbname]
+go
+
+create database [menu-serve] 
